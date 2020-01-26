399350f1a5f37bae8299168c3fda63e41987fb30
cphan31 <cphan31@gatech.edu>
2020-01-25 20:57:44 -0500

    First commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from "react";
 import './App.css';
+import Home from './home/Home.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  constructor(props) {
+      super(props);
+      this.state = { apiResponse: "" };
+  }
+
+  callAPI() {
+      fetch("http://localhost:9000/testAPI")
+          .then(res => res.text())
+          .then(res => this.setState({ apiResponse: res }))
+          .then(console.log(this.state.apiResponse))
+          .catch(err => err);
+  }
+
+  componentDidMount() {
+      this.callAPI();
+  }
+
+  render() {
+      return (
+
+          <div className="App">
+          <p className="App-intro">{this.state.apiResponse}</p>
+          <Home/>
+          </div>
+      );
+  }
 }
+
+
 
 export default App;
---
+++ src/home/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Home.css';
+import Search from '../search/Search.js';
+
+const Home = () => {
+  
+  return (
+    <div>
+    <div id="header">Welcome to huMANAty!</div>
+      <Search/>
+    </div>
+  );
+};
+
+export default Home;
