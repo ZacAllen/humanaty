c06511f921176baf8b723dd8162d3a3fa494592d
Andrew Dallas <ddallas3@gatech.edu>
2020-02-04 20:36:57 -0500

    'Create' button links to new page, made EventCreation.css

--- src/navbar/NavBar.js
+++ src/navbar/NavBar.js
@@ -21,7 +21,7 @@ const NavBar = () => {
               <a class="nav-link js-scroll-trigger">About</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link js-scroll-trigger"  onClick= {() => testCreate()}>Create</a>
+              <a class="nav-link js-scroll-trigger" id = "createEvent" href="/event" onClick= {() => testCreate()}>Create</a>
             </li>
             <li class="nav-item">
               <a class="nav-link" id = "loginClick">Login</a>
