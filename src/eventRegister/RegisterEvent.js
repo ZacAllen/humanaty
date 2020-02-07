dcacd2c8a631504dcb2a96aad45007d97ee77550
Kierra Brigman <brigman.kierra@gmail.com>
2020-02-07 18:04:59 -0500

    copied over css code, will fix later

--- src/eventRegister/RegisterEvent.js
+++ src/eventRegister/RegisterEvent.js
@@ -1,24 +1,192 @@
 import React, { Component } from 'react';
 
+import './RegisterEvent.css';
 import NavBar from '../navbar/NavBar.js';
+import '../../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
+
 
 class RegisterEvent extends Component {
 
     constructor(props) {
-      super(props);
+        super(props);
 
-      // Declare State
-      this.state = {
-      };
+        // Declare State
+        this.state = {
+            value: null
+        };
+    }
+
+
+    nextPage(e) {
+
     }
 
     render() {
-      return (
-        <div id="registerevent">
-          <NavBar/>
-        </div>
-      );
-    }
-  }
+        return (
+            <div id="registerevent">
+                <NavBar/>
 
-  export default RegisterEvent; 
+
+            <div className="inner-container">
+                <div className="header">
+                    Create an Event
+                </div>
+                <div class="thisContainer">
+                    <ul class="progress">
+                        <li class="active"></li>
+                        <li></li>
+                        <li></li>
+                    </ul>
+                </div>
+                <div className = "box">
+                    {/* <div className="labels">
+                        <label htmlFor="name">Event Title</label>
+                    </div>
+                    <div className="input-group">
+                        <input
+                            type="text"
+                            name="name"
+                            className="name-input"
+                            placeholder="My Event"/>
+                    </div>
+                    <div className="labels">
+                        <label htmlFor="name">Street Address*</label>
+                    </div>
+                    <div className="input-group">
+                        <input
+                            type="text"
+                            name="name"
+                            className="name-input"
+                            placeholder=" "/>
+                    </div>
+                    <div className="labels">
+                        <label htmlFor="name">City</label>
+                    </div>
+                    <div className="labels">
+                        <label htmlFor="name">State</label>
+                    </div>
+                    <div className="input-group">
+                        <input
+                        type="text"
+                        name="name"
+                        className="name-input"
+                        placeholder=" "/>
+                        <input
+                            type="text"
+                            name="name"
+                            className="name-input"
+                            placeholder=" "/>
+                    </div>
+                    <div className="labels">
+                        <label htmlFor="name">Postal Code</label>
+                    </div>
+                    <div className="input-group">
+                        <input
+                            type="text"
+                            name="name"
+                            className="name-input"
+                            placeholder=" "/>
+                    </div>
+                    <div className="labels">
+                        <label htmlFor="name">Date</label>
+                    </div>
+                    <div className="labels">
+                        <label htmlFor="name">Time</label>
+                    </div>
+                    <div className="input-group">
+                        <input
+                            type="date"
+                            name="name"
+                            className="name-input"
+                            placeholder=" "/>
+                        <input
+                            type="time"
+                            name="name"
+                            className="name-input"
+                            placeholder=" "/>
+                    </div>
+                    <button
+                        type="button"
+                        className="next"
+                        onClick={this
+                            .nextPage
+                            .bind(this)}>Continue
+                    </button> */}
+
+
+                    <div className = "labels">
+                        <label htmlFor="name">Meal Type</label>
+                    </div>
+                    <select className = "meals" value={this.state.value} onChange={this.handleChange}>
+                        <option value="breakfast">Breakfast</option>
+                        <option value="brunch">Brunch</option>
+                        <option value="lunch">Lunch</option>
+                        <option selected value="dinner">Dinner</option>
+                    </select>
+                    <div className = "labels">
+                        <label htmlFor="name">Number of Guests</label>
+                    </div>
+                    <div className = "input-group">
+                        <input
+                            type="text"
+                            name="guest"
+                            className="guests"></input>
+                    </div>
+                    <div className = "labels">
+                        <label htmlFor="name">Meal Description</label>
+                    </div>
+                    <div className = "input-group">
+                        <input
+                            type="text"
+                            name="meal"
+                            className="meal-input">
+                        </input>
+                    </div>
+                    <button
+                        type="button"
+                        className="next"
+                        onClick={this
+                            .nextPage
+                            .bind(this)}>Continue
+                    </button>
+
+                    {/* Code for the third event screen
+                    <div className="labels">
+                        <label htmlFor="name">Dietary Restrictions</label>
+                    </div>
+                    <div className="input-group">
+                        <input
+                            type="text"
+                            name="name"
+                            className="allergy-input"></input>
+                    </div>
+                    <div className="labels">
+                        <label htmlFor="name">Additional Information</label>
+                    </div>
+                    <div className="input-group">
+                        <input
+                            type="text"
+                            name="name"
+                            className="additional-input"></input>
+                    </div>
+                    <button
+                        type="button"
+                        className="next"
+                        onClick={this
+                            .nextPage
+                            .bind(this)}>Create
+                    </button> */}
+                </div>
+
+            </div>
+                <div className="notice">
+                    <p>*Note your exact location will not be available to
+                        guests until 48 hours before the meal</p>
+                </div>
+            </div>
+        );
+    }
+}
+
+   export default RegisterEvent;
