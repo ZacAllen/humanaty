93291d6da4fd606b95a9825fa2f9e36b3675fa85
Andrew Dallas <ddallas3@gatech.edu>
2020-02-07 03:07:55 -0500

    Wrote code for the second and third event sign up screens (check comments)

--- src/eventCreate/EventCreation.js
+++ src/eventCreate/EventCreation.js
@@ -13,7 +13,7 @@ class EventCreation extends Component {
 
         // Declare State
         this.state = {
-            pathname: "/event"
+            value: null
         };
     }
 
@@ -24,7 +24,9 @@ class EventCreation extends Component {
     // }
 
 
- nextPage(e) {
+
+    nextPage(e) {
+
     }
 
     render() {
@@ -44,7 +46,7 @@ class EventCreation extends Component {
                         <li></li>
                     </ul>
                 </div>
-                <div className="box">
+                <div className = "box">
                     <div className="labels">
                         <label htmlFor="name">Event Title</label>
                     </div>
@@ -129,6 +131,71 @@ class EventCreation extends Component {
                             .nextPage
                             .bind(this)}>Continue
                     </button>
+
+
+                    {/* Code for the second event screen
+                    <div className = "labels">
+                        <label htmlFor="name">Meal Type</label>
+                    </div>
+                    <select className = "meals" value={this.state.value} onChange={this.handleChange}>
+                        <option value="breakfast">Breakfast</option>
+                        <option value="brunch">Brunch</option>
+                        <option value="lunch">Lunch</option>
+                        <option selected value="dinner">Dinner</option>
+                    </select>
+                    <div className = "labels">
+                        <label htmlFor="name">Number of Guests</label>
+                    </div>
+                    <select className = "guests" value={this.state.value} onChange={this.handleChange}>
+                        <option selected value="zero">0</option>
+                        <option value="one">1</option>
+                        <option value="two">2</option>
+                        <option value="three">3</option>
+                    </select>
+                    <div className = "labels">
+                        <label htmlFor="name">Meal Description</label>
+                    </div>
+                    <div className = "input-group">
+                        <input
+                            type="text"
+                            name="meal"
+                            className="meal-input">
+                        </input>
+                    </div>
+                    <button
+                        type="button"
+                        className="next"
+                        onClick={this
+                            .nextPage
+                            .bind(this)}>Continue
+                    </button> */}
+
+                    {/* Code for the third event screen
+                    <div className="labels">
+                        <label htmlFor="name">Dietary Restrictions</label>
+                    </div>
+                    <div className="input-group">
+                        <input
+                            type="text"
+                            name="name"
+                            className="allergy-input"></input>
+                    </div>
+                    <div className="labels">
+                        <label htmlFor="name">Additional Information</label>
+                    </div>
+                    <div className="input-group">
+                        <input
+                            type="text"
+                            name="name"
+                            className="additional-input"></input>
+                    </div>
+                    <button
+                        type="button"
+                        className="next"
+                        onClick={this
+                            .nextPage
+                            .bind(this)}>Create
+                    </button> */}
                 </div>
 
             </div>
