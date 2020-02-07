7ee05c8fa4908d326a3b8524961fb38a90cb8c02
jodiannebartolo <rbartolo3@gatech.edu>
2020-02-06 20:53:05 -0500

    Started work on event creation form

--- src/eventCreate/EventCreation.js
+++ src/eventCreate/EventCreation.js
@@ -1,25 +1,144 @@
 import React, { Component } from 'react';
 
+import './EventCreation.css';
 import NavBar from '../navbar/NavBar.js';
+import '../../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
+
 
 class EventCreation extends Component {
-  
+
     constructor(props) {
-      super(props);
-  
-      // Declare State
-      this.state = {
-        pathname: "/event"
-      };
+        super(props);
+
+        // Declare State
+        this.state = {
+            pathname: "/event"
+        };
     }
-  
+
+    // render() {
+    //     return (
+    //
+    //     );
+    // }
+
+
+ nextPage(e) {
+    }
+
     render() {
-      return (
-        <div id="eventCreation">
-          <NavBar/>
-        </div>
-      );
+        return (
+            <div id="eventCreation">
+                <NavBar/>
+
+
+            <div className="inner-container">
+                <div className="header">
+                    Create an Event
+                </div>
+                <div class="thisContainer">
+                    <ul class="progress">
+                        <li class="active"></li>
+                        <li></li>
+                        <li></li>
+                    </ul>
+                </div>
+                <div className="box">
+                    <div className="labels">
+                        <label htmlFor="name">Event Title</label>
+                    </div>
+                    <div className="input-group">
+
+                        <input
+                            type="text"
+                            name="name"
+                            className="name-input"
+                            placeholder="My Event"/>
+                    </div>
+                    <div className="labels">
+                        <label htmlFor="name">Street Address*</label>
+                    </div>
+                    <div className="input-group">
+
+                        <input
+                            type="text"
+                            name="name"
+                            className="name-input"
+                            placeholder=" "/>
+                    </div>
+                    <div className="labels">
+                        <label htmlFor="name">City</label>
+                    </div>
+                    <div className="labels">
+
+                        <label htmlFor="name">State</label>
+                    </div>
+                    <div className="input-group">
+
+                        <input
+                        type="text"
+                        name="name"
+                        className="name-input"
+                        placeholder=" "/>
+
+                        <input
+                            type="text"
+                            name="name"
+                            className="name-input"
+                            placeholder=" "/>
+                    </div>
+                    <div className="labels">
+                        <label htmlFor="name">Postal Code</label>
+                    </div>
+                    <div className="input-group">
+
+                        <input
+                            type="text"
+                            name="name"
+                            className="name-input"
+                            placeholder=" "/>
+                    </div>
+                    <div className="labels">
+                        <label htmlFor="name">Date</label>
+                    </div>
+                    <div className="labels">
+
+                        <label htmlFor="name">Time</label>
+                    </div>
+                    <div className="input-group">
+
+                        <input
+                            type="date"
+                            name="name"
+                            className="name-input"
+                            placeholder=" "/>
+
+                        <input
+                            type="time"
+                            name="name"
+                            className="name-input"
+                            placeholder=" "/>
+                    </div>
+
+
+                    <button
+                        type="button"
+                        className="next"
+                        onClick={this
+                            .nextPage
+                            .bind(this)}>Continue
+                    </button>
+                </div>
+
+            </div>
+                <div className="notice">
+                    <p>*Note your exact location will not be available to
+                        guests until 48 hours before the meal</p>
+                </div>
+            </div>
+        );
     }
-  }
-  
-  export default EventCreation;
+}
+
+   export default EventCreation;
