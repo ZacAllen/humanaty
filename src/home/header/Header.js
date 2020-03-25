afca05791d400b22b67a45eec5cf4e52711dfb92
cphan31 <cphan31@gatech.edu>
2020-03-24 23:07:06 -0400

    fixed header photo bug

--- src/home/header/Header.js
+++ src/home/header/Header.js
@@ -4,15 +4,14 @@ import '../../../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 const Header = () => {
     return (
-        <div>
-            <header class="bg-primary text-white" id = "header">
-            <div class="container text-center" id = "headerContainer">
-                {/* using inline styling here cuz it wasn't working from css file for some reason. */}
+        <div class="header-container">
+            <header class="bg-primary text-white header-image">
+            <div class="text-center" id = "text-container">
                 <h1 class = "headerTitle" style ={{fontFamily: "verdana-header, sans-serif", fontWeight: "800"}}
                 >Welcome to huMANAty!</h1>
                 <p class="lead" id = "subheader">A website where you can eat farm to table, lorem ipsum, etc etc</p>
             </div>
-        </header>
+            </header>
         </div>        
     );
 };
