9e7d7c05dcd643a7b923c5d0671d33319764de88
Andrew Dallas <ddallas3@gatech.edu>
2020-03-30 18:54:52 -0400

    Added amount to be posted to backend

--- src/payment/Checkout.js
+++ src/payment/Checkout.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 import StripeCheckout from 'react-stripe-checkout';
 import axios from 'axios';
 
-class Checkout extends React.Component{
+class Checkout extends Component{
     constructor(props) {
         super(props);
         this.state = {
@@ -15,17 +15,19 @@ class Checkout extends React.Component{
     }
 
     onToken = (token, addresses) => {
-        var obj = {token: token, addresses: addresses}
+        var obj = {token: token, addresses: addresses, amount: this.props.location.state.amount}
         axios.post('https://localhost:9000/receive-payment', obj);
     }
 
     render() {
         return (
             <StripeCheckout 
-            stripeKey='pk_live_gEj0YgPj0aTtBcjKY3Gp9JnC008ooqLhlH'
+            stripeKey='pk_test_KsSBLHsah3N55vJsAQ7a8YDO00qx7rn3an'
             token={this.onToken}/>
         )
     }
 }
+
+/*Live Key: 'pk_live_gEj0YgPj0aTtBcjKY3Gp9JnC008ooqLhlH'*/
 
 export default Checkout;
